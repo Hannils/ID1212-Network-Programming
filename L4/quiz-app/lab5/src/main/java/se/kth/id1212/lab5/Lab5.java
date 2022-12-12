7699806087cca7b8b26fa9a461ebc0b810ad82d1
Kalle-Elmdahl <75391187+Kalle-Elmdahl@users.noreply.github.com>
2022-12-12 15:13:08 +0100

    L5 start

---
+++ L4/quiz-app/lab5/src/main/java/se/kth/id1212/lab5/Lab5.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package se.kth.id1212.lab5;
+
+import javax.mail.PasswordAuthentication;
+import javax.mail.Authenticator;
+
+import java.io.*;
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+/**
+ *
+ * @author kalleelmdahl
+ */
+public class Lab5 {
+
+    public static void main(String[] args) throws MessagingException, IOException {
+        Console console = System.console();
+        String username = "kelmdahl";
+        String password = "9WpJhN32yHfv";
+        PasswordAuthentication auth = new PasswordAuthentication(username, password);
+        Folder folder = null;
+        Store store = null;
+        try {
+            Properties props = System.getProperties();
+            props.setProperty("mail.store.protocol", "imaps");
+            props.setProperty("mail.imap.port", "993");
+            props.setProperty("mail.imap.host", "webmail.kth.se");
+            props.setProperty("mail.imap.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+            props.setProperty("mail.imap.socketFactory.fallback", "false");
+            props.setProperty("mail.imap.starttls.enable", "true");
+            props.setProperty("mail.debug", "true");
+
+            Session imapSession = Session.getInstance(props);
+
+            store = imapSession.getStore("imap");
+            //Connect to server by sending username and password.
+
+            store.connect("webmail.kth.se", auth.getUserName(), auth.getPassword());
+            //Get all mails in Inbox Forlder
+            folder = store.getFolder("Inbox");
+            folder.open(Folder.READ_ONLY);
+            //Return result to array of message
+            Message messages[] = folder.getMessages();
+            System.out.println("No of Messages : " + folder.getMessageCount());
+            System.out.println("No of Unread Messages : " + folder.getUnreadMessageCount());
+            for (int i = 0; i < messages.length; ++i) {
+                System.out.println("MESSAGE #" + (i + 1) + ":");
+                Message msg = messages[i];
+                System.out.println(msg.getContent());
+            }
+        } finally {
+            if (folder != null) {
+                folder.close(true);
+            }
+            if (store != null) {
+                store.close();
+            }
+        }
+    }
+}
